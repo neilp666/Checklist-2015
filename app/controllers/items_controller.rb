b658d75693a299e5289f437f89b574ed026e28cf
neil patel <neil@myindianweddingapp.com>
2015-10-27 12:29:51 +0000

    Added completed tasks model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end
+
   private 
 
   def item_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027112907) do
+ActiveRecord::Schema.define(version: 20151027120925) do
 
   create_table "items", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151027112907) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: true do |t|
